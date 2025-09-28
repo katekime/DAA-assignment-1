e58b01f50f63ae98022064c79ed5d3d716088caf
katekime <katekime07@gmail.com>
2025-09-28 22:00:48 +0500

    empty main class quicksort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {10, 7, 8, 9, 1, 5};
-        Quicksort sorter = new Quicksort();
-        sorter.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(sorter.getComparisons());
-        System.out.println(sorter.getRecursionDepth());
+
     }
 }
